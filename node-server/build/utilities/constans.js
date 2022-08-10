3df19459df203774b09ce37213133fede34a76b5
Arboleda <juan.arboleda@softwareone.com>
2022-08-10 09:50:45 -0500

    Se elimina el build

--- node-server/build/utilities/constans.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Messages = exports.ApiSetting = void 0;
-exports.ApiSetting = {
-    port: 8080
-};
-exports.Messages = {
-    NoFound: "No se encontraron elementos",
-};
